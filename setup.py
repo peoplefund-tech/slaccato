90ea4c0923b799dd2d462b7d49a36463c1ce599f
Dongho Yu <n0rr7882@gmail.com>
2019-08-29 15:41:08 +0900

    Version up to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='slaccato',
-      version='0.1.12',
+      version='0.2.0',
       description='Structured Slack bot framework.',
       long_description=open('README.md').read(),
       long_description_content_type="text/markdown",
